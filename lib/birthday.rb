0a174022e9312e5fe4df18ae7cb4a6cf928dd022
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-18 21:49:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,9 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-   end
-  en    birthday_kids.each do |kids_name, age|
-      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
- d
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
 
 
